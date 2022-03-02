bba5148e38b94a9524d94dde78f86e12538c91e4
benoson <ben@Bens-MacBook-Pro.local>
2022-03-02 17:59:04 +0200

    Fix search results UI and issue of getting a lot of req

--- woof-server/routers/userRouter.js
+++ woof-server/routers/userRouter.js
@@ -27,4 +27,17 @@ router.post("/login", async (req, res, next) => {
   }
 });
 
+router.get("/search/:searchTerm", async (req, res, next) => {
+  try {
+    const { searchTerm } = req.params;
+    const searchRes = await userService.findUser({
+      name: { $regex: searchTerm, $options: "i" },
+    });
+
+    res.json(searchRes);
+  } catch (error) {
+    console.log("Error logging in: ", error);
+  }
+});
+
 module.exports = router;
